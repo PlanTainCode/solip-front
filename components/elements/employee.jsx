51bdf68e47f406d4594d9776c819112142c10a55
cohohome <cohohome-frontend@ya.ru>
2022-09-01 18:15:39 +0300

    fix team slider on main page

--- components/elements/employee.jsx
+++ components/elements/employee.jsx
@@ -5,7 +5,7 @@ export default function Employee({ name, job, image }) {
     <div className="relative rounded-3xl overflow-hidden h-100 lg:h-122.5" style={{transform: 'translate3d(0, 0, 0)'}}>
       <Image src={image} alt='1' layout='fill' className='block w-full' objectFit='cover' objectPosition='top' style={{transform: 'translate3d(0, 0, 0)'}}/>
 
-      <div className="absolute w-[268px] bottom-0 right-0 bg-silver rounded-tl-3xl p-5 lg:w-75 lg:pt-7 lg:px-8 lg:pb-5">
+      <div className="absolute w-[268px] bottom-0 right-0 bg-silver rounded-tl-3xl p-5 lg:w-75 lg:pt-7 lg:px-8 lg:pb-5" style={{transform: 'translate3d(0, 0, 0)'}}>
         { name && <p className="t-h2">{name}</p> }
         { job && <p className="t-24 mt-1">{job}</p> }
       </div>
