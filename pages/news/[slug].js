c1bb60e5e1f57dbe4b21b655f484b7c65fdbf3a1
cohohome <cohohome-frontend@ya.ru>
2022-09-24 09:23:10 +0300

    set fallback: true

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -46,7 +46,7 @@ export async function getStaticPaths() {
     }
   });
 
-  return { paths, fallback: false }
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
